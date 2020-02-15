96b5a32dfcf2a0eb0c4f7be75ba9e2f65e089643
Tim Donoghue <tmd112593@gmail.com>
2020-02-15 21:02:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,8 +2,12 @@ def speak_to_grandma(phrase)
    
   if phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
+  elsif  "I LOVE YOU GRANDMA!" 
+    return
   else 
     return "HUH?! SPEAK UP, SONNY"
   end 
+end
 
 speak_to_grandma("HI GRAMMY I LOVE YOU")
+
